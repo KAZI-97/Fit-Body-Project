afb5b9cd079fceae82f53036e4bf158061c4c6b6
KAZI-97 <kaziahsanul936@gmail.com>
2026-09-23 20:42:35 +0600

    Navbar Done Without Responsiveness

---
+++ src/app/Components/Shared Components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Logo from '@/assets/logo.png'
+import Link from "next/link";
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="flex justify-between items-center max-w-5xl mx-auto mt-2 p-3">
+                <div className="flex gap-2.5">
+                    <Image src={Logo} alt="Logo Image" width={28} height={28}></Image>
+                    <h1 className={`[var(--font-oswald)] text-[#FFFFFF]`}>FITLOG</h1>
+                </div>
+                <div className={`[var(--font-inter)] flex gap-10`}>
+                    <div>
+                        <Link href=""  className="text-[#c2f800]  hover:w- hover:bg-[#1A2312] hover:p-3.5 hover:rounded-2xl hover:shadow-sm">Workouts</Link>
+                    </div>
+                    <div>
+                       <Link href="" className="text-[#9CA3AF]">My Plan</Link> 
+                    </div>
+                </div>
+                <div className="flex gap-4">
+                    <div className="flex gap-4">
+                        <button className="text-[#D1D5DB]">Plan</button>
+                        <h1>0</h1>
+                    </div>
+                    <div className="flex gap-4">
+                       <button className="text-[#9CA3AF]">Save</button>
+                        <h1>0</h1>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,18 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Components/Shared Components/Navbar";
+import { Inter, Oswald } from "next/font/google";
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const oswald = Oswald({
+  subsets: ["latin"],
+  variable: "--font-oswald",
+});
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,7 +34,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${inter.variable} ${oswald.variable} min-h-full flex flex-col`}>
+        <Navbar></Navbar>
+        {children}
+        
+        </body>
     </html>
   );
 }
